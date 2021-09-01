4600b25b9d7f8286353d2613bc57a1a4acc6d71b
unknown <nmluan.49@gmail.com>
2021-09-02 02:51:38 +0700

    02/09/2021

---
+++ Project/myjavascript.js
@@ -0,0 +1,9 @@
+function SignUp_To_SignIn(){
+    document.getElementById('staticBackdrop').style.display='None';
+    document.getElementById('staticBackdrop1').style.display='block';
+}
+
+function SignIn_To_SignUp(){
+    document.getElementById('staticBackdrop1').style.display='None';
+    document.getElementById('staticBackdrop').style.display='block';
+}
