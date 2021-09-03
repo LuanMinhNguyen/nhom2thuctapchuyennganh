97bbe05a58f367b356435b35c38ba44eb85aeeb7
unknown <nmluan.49@gmail.com>
2021-09-03 22:24:33 +0700

    03/09/2021(3)

---
+++ test-expanding-cards.js
@@ -0,0 +1,13 @@
+function click(){
+    const images = document.querySelectorAll(".image");
+
+    if(images) {
+      images.forEach(image => {
+        image.onclick =() => {
+          document.querySelector(".active").classList.remove("active");
+          image.classList.add("active")
+        }
+      })
+    }
+}
+
