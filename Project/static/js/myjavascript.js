2bda606a9bcfb0dac972f3ae6c4b370a5e31729f
unknown <nmluan.49@gmail.com>
2021-09-11 16:28:29 +0700

    11/09/2021(Final)

--- Project/static/js/myjavascript.js
+++
@@ -1,5 +0,0 @@
-function Hide_replycomment_box(){
-    document.getElementById('hide_replycomment_box').addEventListener('click',()=>{
-        document.getElementById('box-reply-comment').addClass('hide');
-    })
-}
